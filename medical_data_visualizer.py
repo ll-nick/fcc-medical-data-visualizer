530c1b3229fd3c360387bb0b74bfcbc974e9acde
Nick Le Large <nick@le-large.com>
2023-08-16 19:56:52 +0200

    implement cat_plot

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,31 +4,37 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Import data
-df = None
+df = pd.read_csv("medical_examination.csv")
 
 # Add 'overweight' column
-df['overweight'] = None
+df['bmi'] = df['weight'] / ((df['height'] / 100) ** 2)
+df['overweight'] = df['bmi'].apply(lambda bmi: 1 if bmi > 25 else 0)
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
-
+df['cholesterol'] = df['cholesterol'].apply(lambda x: 0 if x == 1 else 1)
+df['gluc'] = df['gluc'].apply(lambda level: 0 if level == 1 else 1)
 
 # Draw Categorical Plot
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = None
-
+    columns_for_plot = ['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight']
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=columns_for_plot, var_name='variable', value_name='value')
 
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
-    df_cat = None
-    
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
 
     # Draw the catplot with 'sns.catplot()'
-
-
+    g = sns.catplot(
+        data=df_cat,
+        kind='bar',
+        col='cardio',
+        x='variable',
+        y='total',
+        hue='value'
+    )
 
     # Get the figure for the output
-    fig = None
-
+    fig = g.fig
 
     # Do not modify the next two lines
     fig.savefig('catplot.png')
