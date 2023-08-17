555bbe3981b4732a84a72a049ffe6f349502cef2
Nick Le Large <nick@le-large.com>
2023-08-17 11:06:16 +0200

    implemented heatmap

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -7,8 +7,8 @@ import numpy as np
 df = pd.read_csv("medical_examination.csv")
 
 # Add 'overweight' column
-df['bmi'] = df['weight'] / ((df['height'] / 100) ** 2)
-df['overweight'] = df['bmi'].apply(lambda bmi: 1 if bmi > 25 else 0)
+bmi = df['weight'] / ((df['height'] / 100) ** 2)
+df['overweight'] = bmi.apply(lambda bmi: 1 if bmi > 25 else 0)
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
 df['cholesterol'] = df['cholesterol'].apply(lambda x: 0 if x == 1 else 1)
@@ -44,22 +44,26 @@ def draw_cat_plot():
 # Draw Heat Map
 def draw_heat_map():
     # Clean the data
-    df_heat = None
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) &
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+        ]
+    df_heat.replace({None: np.nan}, inplace=True)
 
     # Calculate the correlation matrix
-    corr = None
+    corr = df_heat.corr()
 
     # Generate a mask for the upper triangle
-    mask = None
-
-
+    mask = np.triu(np.ones(corr.shape))
 
     # Set up the matplotlib figure
-    fig, ax = None
+    fig, ax = plt.subplots()
 
     # Draw the heatmap with 'sns.heatmap()'
-
-
+    sns.heatmap(corr, annot=True, fmt=".1f", mask=mask)
 
     # Do not modify the next two lines
     fig.savefig('heatmap.png')
